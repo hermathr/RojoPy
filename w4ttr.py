931b5c2b3a0f94f21646672033c29d0b3214d747
Ronnie <hermathr@users.noreply.github.com>
2018-01-22 12:30:08 -0500

    Update w4ttr.py

--- w4ttr.py
+++ w4ttr.py
@@ -5,17 +5,10 @@ yes = True
 a = 0
 
 while yes:
-    print "run", a
-    a = a+1
-    if a == 5:
-        yes = False
 remoteServer = raw_input("Enter a remote host to scan: ")
-p1 = subprocess.Popen(['ifconfig'], stdout=subprocess.PIPE)
-output = p1.communicate()[0]
-print output
 
 if socket.has_ipv6:
-        print "balls"
+        print "Has IPv6"
 
 if not socket.has_ipv6:
-        print "tits"
+        print "Does Not Have IPv6"
